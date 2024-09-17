a37537d8dd2e2882ec5d81330d1a1bd95dcea1ca
Husman Ahmed <husman.ahmed@bjss.com>
2024-09-17 15:50:07 +0100

    add map structure to store and then display data on file close

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import readline from 'node:readline';
 import fs from 'node:fs';
 
+const data = new Map();
+
 const lineReader = readline.createInterface({
   input: fs.createReadStream('measurements_test.txt')
 });
 
+// read file line by line
 lineReader.on('line', function (line) {
   const parts = line.split(';');
 
   const station = parts[0];
   const temperature = parts[1];
 
-  console.log(`station: ${station} temperature: ${temperature}`);
+  data.set(station, temperature);
+});
+
+// log data when file is closed
+lineReader.on('close', () => {
+  data.forEach((temperature, station) => {
+    console.log(`${station};${temperature}`);
+  })
 });
