c40c056949d27c26bac2bb251ebec7d085e833c9
Husman Ahmed <husman.ahmed@bjss.com>
2024-09-17 15:40:52 +0100

    split text to get station and temperature

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import * as readline from 'node:readline';
-import * as fs from 'node:fs';
+import readline from 'node:readline';
+import fs from 'node:fs';
 
 const lineReader = readline.createInterface({
   input: fs.createReadStream('measurements_test.txt')
 });
 
 lineReader.on('line', function (line) {
-  console.log('Line from file:', line);
+  const parts = line.split(';');
+
+  const station = parts[0];
+  const temperature = parts[1];
+
+  console.log(`station: ${station} temperature: ${temperature}`);
 });
